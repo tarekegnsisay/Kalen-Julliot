5155304ce7af80735b9e41ffbd9cf6459a0242c1
Alexis Tanner <alexistanner94@hotmail.com>
2020-01-06 14:16:12 -0500

    minor component structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Container from "./components/Container";
+import Carousel from "./components/Carousel";
+import Form from "./components/Form";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
